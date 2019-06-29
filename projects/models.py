1291e35b3b8d4ce151c388d409357f7fc40511f0
Roopak Kumar Prajapat <roopakkprajapat@Roopaks-MacBook-Pro.local>
2019-06-29 14:16:14 +0530

    project setup

--- projects/models.py
+++ projects/models.py
@@ -1,8 +1,14 @@
 from django.db import models
+from django_countries.fields import CountryField
 
 class Project(models.Model):
     name = models.CharField(max_length=100, blank=False)
     status = models.CharField(max_length=50, blank=False)
+    address1 = models.CharField(max_length=254, blank=False)
+    address2 = models.CharField(max_length=254, blank=True)
+    city = models.CharField(max_length=50, blank=False)
+    zip_code = models.CharField('ZIP/Postal Code', max_length=12)
+    country = CountryField()
     image = models.ImageField(blank=True)
 
     def __str__(self):
