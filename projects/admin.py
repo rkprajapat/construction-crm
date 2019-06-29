0320540e2205945c8d6548f62542a0eff2cf207f
Roopak Kumar Prajapat <roopakkprajapat@Roopaks-MacBook-Pro.local>
2019-06-29 14:58:50 +0530

    added project entities

--- projects/admin.py
+++ projects/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 
-from .models import Project
+from .models import Project, Tower, Unit
 
 class ProjectAdmin(admin.ModelAdmin):
     pass
 
 admin.site.register(Project, ProjectAdmin)
+admin.site.register(Tower, ProjectAdmin)
+admin.site.register(Unit, ProjectAdmin)
--- projects/models.py
+++ projects/models.py
@@ -4,12 +4,29 @@ from django_countries.fields import CountryField
 class Project(models.Model):
     name = models.CharField(max_length=100, blank=False)
     status = models.CharField(max_length=50, blank=False)
-    address1 = models.CharField(max_length=254, blank=False)
-    address2 = models.CharField(max_length=254, blank=True)
+    address1 = models.CharField('Address Line 1', max_length=254, blank=False)
+    address2 = models.CharField('Address Line 2', max_length=254, blank=True)
     city = models.CharField(max_length=50, blank=False)
-    zip_code = models.CharField('ZIP/Postal Code', max_length=12)
+    zip_code = models.PositiveSmallIntegerField('ZIP/Postal Code')
     country = CountryField()
     image = models.ImageField(blank=True)
 
     def __str__(self):
         return self.name
+
+
+class Tower(models.Model):
+    name = models.CharField(max_length=20, blank=False)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return ' '.join([self.project.name, '-', self.name])
+
+
+class Unit(models.Model):
+    unit = models.PositiveSmallIntegerField(blank=False)
+    project_tower = models.ForeignKey(Tower, on_delete=models.CASCADE)
+    status = models.CharField(max_length=20, blank=False)
+
+    def __str__(self):
+        return ' '.join([self.project_tower.project.name, self.project_tower.name, str(self.unit)])
